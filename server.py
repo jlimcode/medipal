860b173f15a5cb93664cbd7a8e905a2bd51e5eeb
jlimcode <56215940+jlimcode@users.noreply.github.com>
2021-04-27 19:39:54 -0700

    Send simple messages

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
+from twilio.rest import Client
+import os
 from flask import Flask
 from flask import request
 app = Flask(__name__)
 
+# Download the helper library from https://www.twilio.com/docs/python/install
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_number = os.getenv('TWILIO_NUMBER')
+client = Client(account_sid, auth_token)
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/ping', methods=['GET', 'POST'])
 def send_text():
@@ -13,4 +26,10 @@ def send_text():
         print("Invalid number")
     else:
         print(number)
+        message = client.messages.create(
+            body="Hello world!",
+            from_= twilio_number,
+            to='+' + number
+        )
+        print(message.sid)
     return 'Your number was ' + number
