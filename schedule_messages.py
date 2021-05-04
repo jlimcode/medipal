010ac421a8673f0ac98b135f465a2ab087d1bb9b
Julia Offerman <lia.offerman@gmail.com>
2021-05-03 21:59:47 -0700

    Checking if time to notify patient
    Computes TIME_INTERVAL minute range from the current time and
    checks if any of the medications in the database have a dose
    time in that interval.

--- schedule_messages.py
+++ schedule_messages.py
@@ -1,26 +1,44 @@
 import os
+import time
 from pymongo import MongoClient
+
+TIME_INTERVAL = 10 
+
 mongo_key = os.getenv('MONGO_LOGIN')
 
 cluster = MongoClient(mongo_key)
 db = cluster["Medipal"]
-Users = db["Users"] # Collection of users with a list of medicines
-Meds = db["Meds"] # Collection of medicines with list of times, chronic flag,
-                  # decrementing doses left (-1 if chronic), and message
-                  # structure can be changed
+Users = db["Users"] 
+Meds = db["Meds"] 
 
-# schedule every TIME_INTERVAL minutes
+meds_list = Meds.find({})
 
-    # get time here
-    meds_list = Meds.find({})
+now = time.localtime(time.time())
+hour = now.tm_hour
+mins = now.tm_min
+timeOptions = []
 
-    for m in meds_list:
+# calcuating and formating range of minutes in TIME_INTERVAL from current time in a list
+if (mins < (60 - TIME_INTERVAL)):
+    for x in range(mins, mins+ TIME_INTERVAL + 1): 
+        t = str(hour) + ":" + str(x)
+        timeOptions += [t]
+else:
+    over = mins - 60 + TIME_INTERVAL
+    for x in range(mins, 60):
+        t = str(hour) + ":" + str(x)
+        timeOptions += [t]
+    next_h = hour + 1
+    if hour == 23:
+        next_h = 0
+    for x in range(0, over+1):
+        t = str(next_h) + ":" + str(x)
+        timeOptions += [t]
+
+for m in meds_list:
         for time in m["times"]:
-            # if time is in the interval:
-                # construct message
-                # call send_message accordingly
-                if ! (m["chronic"]):
-                    # decrement m["remDoses"]
-
-
-        
+            if time in timeOptions:
+                print("send message" + m["_id"]) # TODO: print USER id too (need to restructure db to store this)
+                # call send_message.py
+            if not (m["chronic"]):
+                print("not chronic" + m["_id"]) # TODO: decremend remDoses here
