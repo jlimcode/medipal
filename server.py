51237c1879db74b092927b565f9b389dcc3a3e1b
Jason Lim <jmlim@pm.me>
2021-05-08 19:32:13 -0700

    Upload csv to Dictionary
    Added a page that accepts CSV upload and converts to Python dictionary. Need to customize for specific csv fields.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 import os
-from flask import Flask
-from flask import request
+import csv
+from flask import Flask, request, redirect, flash
 app = Flask(__name__)
 
 # Download the helper library from https://www.twilio.com/docs/python/install
@@ -33,3 +33,41 @@ def send_text():
         )
         print(message.sid)
     return 'Your number was ' + number
+
+
+ALLOWED_EXTENSIONS = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            csv_text = "".join(list(file.read().decode('utf-8')))
+            csv_reader = csv.DictReader(csv_text.splitlines(), delimiter=',')
+            for line_count, row in enumerate(csv_reader):
+                if line_count == 0:
+                    print(f'Column names are {", ".join(row)}')
+                print(line_count)
+        return 'Success!'
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
