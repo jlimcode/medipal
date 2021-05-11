6e65368fa8de5f94e9d739a06c033d62f0239ca9
Jason Lim <jmlim@pm.me>
2021-05-10 23:33:33 -0700

    Add schedule comment

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,6 @@ from crontab import CronTab
 
 TIME_INTERVAL = 10
 cron = CronTab(user='root')
-job = cron.new(command='python send_messages.py')
+job = cron.new(command='python send_messages.py', comment='medipal_sendtexts')
 job.minute.every(TIME_INTERVAL)
 cron.write()
