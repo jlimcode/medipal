89ebda83ba07c6485cb47519201c64a513efade2
oliviaapuzzio <68409588+oliviaapuzzio@users.noreply.github.com>
2021-05-15 11:47:22 -0700

    Text message send function

---
+++ send_text.py
@@ -0,0 +1,22 @@
+import os
+from twilio.rest import Client
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+twilio_number = os.getenv('TWILIO_NUMBER')
+client = Client(account_sid, auth_token)
+
+
+def send_text(number, message):
+    """
+    param: number is given without "+"
+    """
+    if number is None:
+        print("Invalid number")
+    else:
+        message = client.messages.create(
+            body=message,
+            from_=twilio_number,
+            to='+' + number
+        )
+        print(message.sid)
+    return 0
